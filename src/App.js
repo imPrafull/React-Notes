30b85a6cd607cdac9e2feb97a8d47769dc84c3e0
Prafull Sakpal <prafullsakpal158@gmail.com>
2020-08-03 19:33:13 +0530

    mapping arrays

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import TodoItem from './components/TodoItem'
+
+import TodoItem from './components/TodoItem';
+import todoData from './todoData'
 
 function App() {
+    const todoItems = todoData.map(item => <TodoItem key={item.id} item={item}/>)
     return (
         <div className="todo-list">
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
-            <TodoItem />
+            {todoItems}
         </div>
     )
 }
---
+++ src/todoData.js
@@ -0,0 +1,29 @@
+const todoData = [
+    {
+        id: 1,
+        text: 'Take out the trash.',
+        completed: true
+    },
+    {
+        id: 2,
+        text: 'Grocery shopping.',
+        completed: false
+    },
+    {
+        id: 3,
+        text: 'Clean the car.',
+        completed: false
+    },
+    {
+        id: 4,
+        text: 'Mow lawn.',
+        completed: true
+    },
+    {
+        id: 5,
+        text: 'Catch up on Money Heist.',
+        completed: false
+    },
+];
+
+export default todoData
