f298ca0a0778519eb81873e7a1a46e4b7e2e87a1
Prafull Sakpal <prafullsakpal158@gmail.com>
2020-08-05 19:51:06 +0530

    store todo data in state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import TodoItem from './components/TodoItem';
 import todoData from './todoData'
 
-function App() {
-    const todoItems = todoData.map(item => <TodoItem key={item.id} item={item}/>)
-    return (
-        <div className="todo-list">
-            {todoItems}
-        </div>
-    )
+class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            todos: todoData
+        }
+    }
+
+    render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} />)
+        return (
+            <div className="todo-list">
+                {todoItems}
+            </div>
+        )
+    }
 }
 
 export default App
