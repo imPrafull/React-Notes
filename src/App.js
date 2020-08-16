ab8ee546e4613782beccc6605190e25c21e0be46
Prafull Sakpal <prafullsakpal158@gmail.com>
2020-08-16 19:42:03 +0530

    checkbox change state

--- src/App.js
+++ src/App.js
@@ -10,15 +10,30 @@ class App extends Component {
         this.state = {
             todos: todoData
         }
+        this.handleChange = this.handleChange.bind(this);
     }
 
     render() {
-        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} />)
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange} />)
         return (
             <div className="todo-list">
                 {todoItems}
             </div>
         )
+    }
+
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed;
+                }
+                return todo;
+            })
+            return {
+                todos: updatedTodos
+            }
+        });
     }
 }
 
